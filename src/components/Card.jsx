1b57535683b80db243f592079e83205461d4700b
florian <florianbarjon@icloud.com>
2024-12-24 14:11:46 +0100

    Initial commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Card = ({meal}) => {
+
+
+    return (
+      <div className="meal-card">
+        <h2>{meal.strMeal}</h2>
+        <p>Origin : {meal.strArena}</p>
+        <img src={meal.strMealThumb} alt={"photo" + meal.strMeal} />
+        <p>{meal.strInstructions}</p>
+      </div>
+    );
+};
+
+export default Card;
